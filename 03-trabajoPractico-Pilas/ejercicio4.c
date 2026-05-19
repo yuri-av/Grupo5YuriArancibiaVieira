be4f749b14a23266b47fa79a8f5f414dc64e41b2
yuri-av <arancibiavieirayuri@gmail.com>
2026-05-19 13:34:41 -0300

    4

---
+++ 03-trabajoPractico-Pilas/ejercicio4.c
@@ -0,0 +1,27 @@
+/*Construir un algoritmo que, utilizando el TAD Pila, permita convertir un número
+decimal pasado como parámetro, a su correspondiente valor expresado en una base
+de 2 a 16 (hexadecimal). Determinar la complejidad algorítmica de la solución.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "pilas.h"
+#include "tipo_elemento.h"
+
+char *p_ej4_cambiarbase(int nrobasedecimal, int nrootrabase);
+
+int main()
+{
+    // Crear validacion de rango entre 2 y 16
+    // Pedir datos numericos de base y nueva base
+
+    return 0;
+}
+
+char *p_ej4_cambiarbase(int nrobasedecimal, int nrootrabase)
+{
+    // Crear pila de nuevo numero como string
+    // ir dividiendo el ultimo numero del nrobasedcimal por nrootrabase**n
+    // cada uno de esos resultados va a la nueva pila
+    Pila resultado = p_crear();
+}
