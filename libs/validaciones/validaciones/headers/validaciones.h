3ad2c96073b24a1cf481ebdbd19baf50f2c81097
IvoMarchiCastro <ivo.marchi.castro1@gmail.com>
2026-06-28 21:25:54 -0300

    Agrego de funcionalidades para validaciones

---
+++ libs/validaciones/validaciones/headers/validaciones.h
@@ -0,0 +1,24 @@
+#ifndef VALIDACIONES_H
+#define VALIDACIONES_H
+
+#include <stdbool.h>
+
+//validacion para la seleccion de los swtich 
+char validacionDeLetra(char mensaje[], char opcionStr[], int tamOpcion, char inicio, char fin);
+
+//validacion general para un numero entero
+int validacion_ingreso();
+
+//Herramienta para textos
+void leerCadena(const char *mensaje, char *buffer, int limite);
+
+//Herramientas para enteros
+int leerEntero(const char *mensaje, bool permitirNegativos);
+
+//validacion para double int
+double validacion_ingreso_double();
+//Validacion para int
+void ValidacionEntero(int *clave, char Msg[]);
+
+void Cartelito(char Msg[],char Separador[]);
+#endif 
